4c78624a4366cbbaa5507e1aa7153670067658cc
falaprodavi <falaprodavi@gmail.com>
2022-01-29 19:40:50 -0300

    Aula Final

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './Footer.css'
+
+export default () => {
+    return (
+
+            <footer>
+Curso de React - Projeto Clone do Netflix  <br/>
+Dados pegos do site Themoviedb.org <br/>
+Instrutor: Bonieky Lacerda<br/>
+            </footer>
+
+
+    )
+}
